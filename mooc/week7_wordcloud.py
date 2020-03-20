9c98afd9a7618a24302da48b14b2c7bb0dc5eedd
SYN-HOME <1845018275@qq.com>
2020-03-20 14:35:06 +0800

    SYN-HOME

--- mooc/week7_wordcloud.py
+++ mooc/week7_wordcloud.py
@@ -2,7 +2,7 @@
 # 生成工作会议的词云
 import jieba
 import wordcloud
-f = open("../data/中心第一次工作会议.txt", "r", encoding="utf-8")
+f = open("D:/MyCode/PythonToLearn/data/中心第一次工作会议.txt", "r", encoding="utf-8")
 
 t = f.read()
 f.close()
@@ -18,7 +18,7 @@ w = wordcloud.WordCloud( \
     max_words = 20
 )
 w.generate(txt)
-w.to_file("D:/Code/PythonToLearn/file/grwordcloud.png")
+w.to_file("D:/MyCode/PythonToLearn/creat/grwordcloud.png")
 print("图片已生成")
 
 # "做好","有关","确保","牵头","要求","继续","各项","工作","到位"
